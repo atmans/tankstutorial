c3661722343c1a07d409618c02d52e20d0ee04f3
Arpana Mansfield <silencethegame@hotmail.com>
2016-11-05 10:19:51 -0700

    Initial commit

---
+++ Tank/Source/Tank/TankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_API ATankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
